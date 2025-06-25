e60ee565f076981d0d43a519007057c6a949d580
guizera0701 <guilhermealves2498@gmail.com>
2025-06-25 16:54:50 -0300

    tela de apresentacao

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+
+//funcao cadastrar funcionario 
+//funcao imprimir nota fiscal
+//funcao imprimir atestado
+
+
 int main(){
     // guilherme faça a tela de apresentação
+printf("============================================================================");
+printf("=                                Clinica CRE                               =");
+printf("=          'Centro de Reabilitação Esportivo Profissional e Amador'        =");
+printf("============================================================================");
+
+
+
 }
