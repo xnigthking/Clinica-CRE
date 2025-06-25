e3171c009f270d7b274c86a28cc94c074c33ed73
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-06-25 17:38:26 -0300

    credenciamento

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 // funcao cadastrar funcionario
 // funcao imprimir nota fiscal
 // funcao imprimir atestado
@@ -9,30 +11,32 @@ int main()
     char codigoFuncionario[50];
     char senha[50];
 
+    system("cls");
     printf("============================================================================\n");
-    printf("=                                Clinica CRE                               =\n");
-    printf("=          'Centro de Reabilitação Esportivo Profissional e Amador'        =\n");
+    printf("                                Clinica CRE                                 \n");
+    printf("         'Centro de Reabilitação Esportivo Profissional e Amador'           \n");
     printf("============================================================================\n");
 
     printf("\n");
     printf("digite seu codigo: ");
-    scanf("%s", &codigoFuncionario);
+    fgets(codigoFuncionario, sizeof(codigoFuncionario), stdin);
+    strtok(codigoFuncionario, "\n");
     printf("digite a sua senha: ");
-    scanf("%s", &senha);
-    getchar();
+    fgets(senha, sizeof(senha), stdin);
+    strtok(senha, "\n"); 
 
-    if (codigoFuncionario == "rep123" && senha == "rep123")
+    if (strcmp(codigoFuncionario, "rep123") == 0 && strcmp(senha, "rep123") == 0)
     {
         printf("Area recepcao! ");
     }
-    else if (codigoFuncionario == "medic123" && senha == "medic123")
+    else if (strcmp(codigoFuncionario, "medic123") == 0 && strcmp(senha, "medic123") == 0)
     {
         printf("Area do medico! ");
     }
     else
     {
-        printf("Senha ou login incorreto!");
+        printf("Senha ou login incorreto!\n");
     }
-    printf("Sistema finalizado");
+    printf("Sistema finalizado\n");
     return 0;
 }
